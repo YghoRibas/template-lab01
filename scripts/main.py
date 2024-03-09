e1c6c37cbd71f6126b623f69541f564827a5dd4e
Yghor Gomes <55198336+YghoRibas@users.noreply.github.com>
2024-03-09 11:58:30 -0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,70 +1,103 @@
 import requests
 from datetime import datetime
 
-token = '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'
+token = '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'
 
 url = 'https://api.github.com/graphql'
 
-query = """
-{
-  search(query: "stars:>1", type: REPOSITORY, first: 1000) {
-    edges {
-      node {
-        ... on Repository {
-          name
-          createdAt
-          updatedAt
-          pullRequests(states: MERGED) {
-            totalCount
+
+def run_query(cursor=None):
+  query = """
+    query ($cursor: String) {
+      search(query: "stars:>1", type: REPOSITORY, first: 100, after: $cursor) {
+        edges {
+          node {
+            ... on Repository {
+              name
+              pullRequests(states: MERGED) {
+                totalCount
+              }
+              primaryLanguage {
+                name
+              }
+              issues {
+                totalCount
+              }
+              issuesClosed: issues(states: CLOSED) {
+                totalCount
+              }
+              updatedAt
+              createdAt
+            }
           }
-          primaryLanguage {
-            name
-          }
-          issues {
-            totalCount
-          }
-          issuesClosed: issues(states: CLOSED) {
-            totalCount
-          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
         }
       }
     }
+    """
+
+  headers = {
+      'Authorization': f'Bearer {token}',
   }
-}
-"""
 
-headers = {
-    'Authorization': f'Bearer {token}',
-}
+  response = requests.post(url,
+                           json={
+                               'query': query,
+                               'variables': {
+                                   'cursor': cursor
+                               }
+                           },
+                           headers=headers)
 
-response = requests.post(url, json={'query': query}, headers=headers)
+  if response.status_code == 200:
+    data = response.json()
+    repositories = data['data']['search']['edges']
+    page_info = data['data']['search']['pageInfo']
+    return repositories, page_info['endCursor'] if page_info[
+        'hasNextPage'] else None
+  else:
+    print(f'Erro na requisição: {response.status_code}, {response.text}')
+    return None, None
 
-if response.status_code == 200:
-  data = response.json()
 
-  print(f'Relatório:')
-  repositories = data['data']['search']['edges']
-  for repo in repositories:
+all_repositories = []
+cursor = None
+
+for i in range(10):
+  repositories, cursor = run_query(cursor)
+  if not repositories:
+    break
+  all_repositories.extend(repositories)
+
+with open('resultados.txt', 'w') as file:
+  file.write(
+      f'Nome, Idade, PRs, ultima atualização, razão de issues, linguagem principal\n'
+  )
+  for repo in all_repositories:
     name = repo['node']['name']
     created_at = repo['node']['createdAt']
-    PRs = repo['node']['pullRequests']['totalCount']
     updated_at = repo['node']['updatedAt']
-    primary_language = repo['node']['primaryLanguage']['name'] if repo['node']['primaryLanguage'] else 'null'
+    PRs = repo['node']['pullRequests']['totalCount']
+    primary_language = repo['node']['primaryLanguage']['name'] if repo['node'][
+        'primaryLanguage'] else None
 
     closed_issues = repo['node']['issuesClosed']['totalCount']
     total_issues = repo['node']['issues']['totalCount']
 
-    issues_relationship = closed_issues / total_issues if total_issues > 0 else 0
+    issue_relationship = closed_issues / total_issues if total_issues > 0 else 0
 
-    current_date = datetime.now()
-    
     created_date = datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%SZ')
-    age = (current_date - created_date).days/365
+    current_date = datetime.now()
 
     updated_date = datetime.strptime(updated_at, '%Y-%m-%dT%H:%M:%SZ')
     updated_date_formatted = updated_date.strftime('%d/%m/%Y')
 
-    print(f'{name}, {age:.0f}, {PRs}, {updated_date_formatted}, {primary_language}, {issues_relationship:.2f}')
+    age_years = (current_date - created_date).days / 365
 
-else:
-  print(f'Erro na requisição: {response.status_code}, {response.text} ')
+    result_line = f' {name}, {age_years:.0f},  {PRs}, {updated_date_formatted}, {issue_relationship: .2f}, {primary_language}\n'
+    file.write(result_line)
+
+print("Resultados salvos em 'resultados.txt'")
