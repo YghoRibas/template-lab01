dca7a551f793f55db5e6a0a63badbd5146da0d34
Yghor Gomes <55198336+YghoRibas@users.noreply.github.com>
2024-03-13 22:27:01 -0300

    Update sumario.py

--- scripts/sumario.py
+++ scripts/sumario.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('./dataset/resultadosq2.txt', skiprows=1)
 
-segundo_indice_coluna = 1
-
 sumarized_data = df.groupby(df.columns[1]).agg({
     0: 'count', 
     2: 'sum',  
